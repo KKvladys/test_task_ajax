d190f85f77d221a81ff5697100cc26c18d976d40
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-03-02 19:02:13 +0200

    refactored sensor_log_parser.py

--- task1/sensor_log_parser.py
+++ task1/sensor_log_parser.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from typing import Generator
 
 ERROR_CODES = {
     0: "Battery device error",
@@ -14,7 +15,8 @@ S_P2_INDEX = 13
 SENSOR_STATE_OK = "02"
 SENSOR_STATE_FAILED = "DD"
 
-def read_log_file(file_path: str) -> list:
+
+def read_log_file(file_path: str) -> Generator[str, None, None]:
     with open(file_path, "r", encoding="utf-8") as log_file:
         for line in log_file:
             if "BIG" in line:
@@ -25,7 +27,6 @@ def parse_log_message(line: str) -> tuple:
     """
     Parses one line of a log file into parts.
     """
-
     parts = line.rsplit(">", 1)[-1].strip(" '\n").rstrip(";").split(";")
 
     sensor_id = parts[SENSOR_ID_INDEX]
@@ -36,30 +37,28 @@ def parse_log_message(line: str) -> tuple:
     return sensor_id, state, s_p1, s_p2
 
 
-def process_sensor_data(sensor_data: list) -> tuple:
+def process_sensor_data(file_path: str) -> tuple:
     """
     Processes sensor data, identifies successful and unsuccessful sensors.
     """
-    sensor_ok = defaultdict(int)
-    sensor_failed = {}
+    sensors_ok = defaultdict(int)
+    sensors_failed = {}
 
-    for line in sensor_data:
-        parsed_data = parse_log_message(line)
+    for line in read_log_file(file_path):
+        sensor_id, state, s_p1, s_p2 = parse_log_message(line)
 
-        sensor_id, state, s_p1, s_p2 = parsed_data
-
-        if sensor_id in sensor_failed:
+        if sensor_id in sensors_failed:
             continue
 
         if state == SENSOR_STATE_FAILED:
-            sensor_failed[sensor_id] = (s_p1, s_p2)
-            sensor_ok.pop(sensor_id, None)
+            sensors_failed[sensor_id] = (s_p1, s_p2)
+            sensors_ok.pop(sensor_id, None)
             continue
 
         if state == SENSOR_STATE_OK:
-            sensor_ok[sensor_id] += 1
+            sensors_ok[sensor_id] += 1
 
-    return sensor_ok, sensor_failed
+    return sensors_ok, sensors_failed
 
 
 def decode_error_flags(sensor_failed: dict) -> None:
@@ -80,25 +79,24 @@ def decode_error_flags(sensor_failed: dict) -> None:
 
 
 def display_results(
-        sensor_ok: dict,
-        sensor_failed: dict,
+        sensors_ok: dict,
+        sensors_failed: dict,
 ) -> None:
-    successful_sensors, failed_sensors = len(sensor_ok), len(sensor_failed)
+    successful_sensors, failed_sensors = len(sensors_ok), len(sensors_failed)
 
     print(f"ALL big messages: {successful_sensors + failed_sensors}")
     print(f"Successful big messages: {successful_sensors}")
     print(f"Failed big messages: {failed_sensors}\n")
 
-    decode_error_flags(sensor_failed)
+    decode_error_flags(sensors_failed)
 
     print("\nSuccess messages count:")
-    for sensor_id, count in sensor_ok.items():
+    for sensor_id, count in sensors_ok.items():
         print(f"{sensor_id}: {count}")
 
 
 def process_logs(file_path: str) -> None:
-    sensor_data = read_log_file(file_path)
-    sensor_ok, sensor_failed = process_sensor_data(sensor_data)
+    sensor_ok, sensor_failed = process_sensor_data(file_path)
     display_results(sensor_ok, sensor_failed)
 
 
