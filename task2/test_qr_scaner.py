59eb1069346a8493f970a5f6ee30b5ad3d4e11c0
Vladyslav Kucherenko <vlados.soc@gmail.com>
2025-03-02 13:51:57 +0200

    add test to check error called

--- task2/test_qr_scaner.py
+++ task2/test_qr_scaner.py
@@ -42,3 +42,14 @@ def test_check_scanned_device_not_in_db(mock_check_in_db, check_qr):
     with patch.object(check_qr, "send_error") as mock_send_error:
         check_qr.check_scanned_device(qr_code)
         mock_send_error.assert_called_once_with("Not in DB")
+
+
+@pytest.mark.parametrize("qr_code, expected_error", [
+    ("abcd", "Error: Wrong qr length 4"),
+    ("xy", "Error: Wrong qr length 2"),
+])
+@patch.object(CheckQr, "check_in_db", return_value=None)
+def test_send_error_called(mock_check_in_db, check_qr, qr_code, expected_error):
+    with patch.object(check_qr, "send_error") as mock_send_error:
+        check_qr.check_scanned_device(qr_code)
+        mock_send_error.assert_any_call(expected_error)
